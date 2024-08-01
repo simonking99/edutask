ccdadbdcd2ab1e53448ff8d1c34e3e45b94698f3
simonking99 <sibo18@student.bth.se>
2024-08-01 16:04:20 +0200

    Updated test_usercontroller.py

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -2,59 +2,75 @@ import pytest
 import unittest.mock as mock
 from src.controllers.usercontroller import UserController
 
+
 @pytest.mark.unit
 def test_valid_email_with_no_user():
-    # Mocking the dependencies
-    mocked_usercontroller = mock.MagicMock()
-    # Create one user for the email
-    mocked_usercontroller.find.return_value = []
+    # Skapar en mock av usercontroller, så vi kan kontrollera dess beteende under testet
+    mocked_uc = mock.MagicMock()
+
+    # Ställer in mocken att returnera en tom lista när find-metoden anropas (Alltså ingen user)
+    mocked_uc.find.return_value = []
     
-    # Creating UserController instance with mocked data_object
-    sut = UserController(dao=mocked_usercontroller)
+    # Skapar en instans av UserController med den mockade dao (data access object)
+    sut = UserController(dao=mocked_uc)
     
-    # Call the method under test
-    validation_result = sut.get_user_by_email('test4444@test.com')
+    # Anropar get_user_by_email-metoden med en giltig e-postadress som inte finns i databasen
+    validation_result = sut.get_user_by_email('test@gmail.com')
+    
+    # Kontrollerar att resultatet är None om ingen user hittas
     assert validation_result is None
 
 @pytest.mark.unit
 def test_valid_email_with_single_user():
-    # Mocking the dependencies
-    mocked_usercontroller = mock.MagicMock()
-    # Create one user for the email
-    mocked_usercontroller.find.return_value = [{"User": "user1" , "email": 'test@test.com'}]
+    mocked_uc = mock.MagicMock()
+
+    # Ställer in mocken att returnera en email som har en user
+    mocked_uc.find.return_value = [{"User": "testuser", "email": 'test@gmail.com'}]
+
+    sut = UserController(dao=mocked_uc)
+
+    # Anropar get_user_by_email-metoden med en giltig e-postadress
+    validation_result = sut.get_user_by_email('test@gmail.com')
     
-    # Creating UserController instance with mocked data_object
-    sut = UserController(dao=mocked_usercontroller)
-    
-    # Call the method under test
-    validation_result = sut.get_user_by_email('test@test.com')
-    # Assert the result
+    #Kontrollerar att user finns
     assert validation_result
 
 @pytest.mark.unit
 def test_valid_email_with_multiple_users():
-    # Mocking the dependencies
-    mocked_usercontroller = mock.MagicMock()
-    # Create multiple users for the email
-    mocked_usercontroller.find.return_value = [{"User": "user1" , "email": 'test@test.com'}, {"User": "user2" , "email": 'test@test.com'}, {"User": "user3" , "email": 'test@test.com'}]
-    
-    # Creating UserController instance with mocked data_object
-    sut = UserController(dao=mocked_usercontroller)
-    
-    # Call the method under test
-    validation_result = sut.get_user_by_email('test@test.com')
-    # Assert the result
+    mocked_uc = mock.MagicMock()
+
+    # Ställer in mocken att returnera en email med flertal users
+    mocked_uc.find.return_value = [
+        {"User": "user1", "email": 'test@gmail.com'},
+        {"User": "user2", "email": 'test@gmail.com'}
+    ]
+    sut = UserController(dao=mocked_uc)
+    validation_result = sut.get_user_by_email('test@gmail.com')
+
+    #Kontrollerar att resultat är den första user
     assert validation_result
 
 @pytest.mark.unit
-def test_unvalid_email():
-    mocked_usercontroller = mock.MagicMock()
-    # Mocking the dependencies
-    mocked_usercontroller.find.return_value = [{'email': 'unvalid_email'}]
-    # Creating invalid email
+def test_invalid_email():
+    mocked_uc = mock.MagicMock()
 
-    # Creating UserController instance with mocked data_object
-    sut = UserController(dao=mocked_usercontroller)
-    # Call the method under test and assert that it raises a ValueError
-    with pytest.raises(ValueError):
-        sut.get_user_by_email('unvalid_email')
+    # Ställer in mocken att returnera en email med flertal users
+    mocked_uc.find.return_value = []
+    sut = UserController(dao=mocked_uc)
+
+    # Anropar get_user_by_email-metoden med en ogiltig e-postadress
+    validation_result = sut.get_user_by_email('1234')
+
+    assert validation_result
+
+@pytest.mark.unit
+def test_exception():
+    mocked_dao = mock.MagicMock()
+
+    # Konfigurerar mocken så att find-metoden kastar ett Exception när den anropas
+    mocked_dao.find.side_effect = Exception()
+    mocked_uc = UserController(dao=mocked_dao)
+
+    #Testet kontrollerar att get_user_by_email-metoden kastar ett undantag av typen Exception när den anropas med mailen
+    with pytest.raises(Exception):
+        mocked_uc.get_user_by_email('test@gmail.com')
