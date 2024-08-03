79120c6dd88da86c9a9a8b8b53863bf37571b236
simonking99 <sibo18@student.bth.se>
2024-08-03 21:06:09 +0200

    Updated test_dao.py

--- backend/test/unit/test_dao.py
+++ backend/test/unit/test_dao.py
@@ -1,98 +1,53 @@
 import pytest
-from unittest.mock import MagicMock
-import unittest.mock as mock
+from unittest.mock import patch, MagicMock
+from pymongo import MongoClient
+from pymongo.errors import WriteError
+import json
 from src.util.dao import DAO
-from dotenv import dotenv_values
-import os
-import pymongo
-
-# Fixture to initialize DAO with a temporary collection for testing
-@pytest.fixture
-def dao():
-    ''' Initialization of the dao with the user collection '''
-    collection_name = "user"
-    dao = DAO(collection_name)
-    dao.client = MagicMock()  # Using a MagicMock for mocking the database connection
-    yield dao
+# Path to the validator file
+file_path = '/root/winhome/system/edutask/backend/src/static/validators/user.json'
 
 @pytest.fixture
-def db():
-    ''' Connection to database '''
-    # Mock the MongoDB client
-    mock_client = MagicMock(spec=pymongo.MongoClient)
-    return mock_client
+def sut():
+    # Read from json file
+    with open(file_path, 'r') as file:
+        json_f = json.load(file)
+
+    with patch('src.util.dao.getValidator', autospec=True) as mock_Validator:
+        mock_Validator.return_value = json_f
+
+        sut = DAO("user")
+        
+        yield sut
+
+    client = MongoClient('mongodb://root:root@localhost:27017')
+    db = client['edutask']
+    db['user'].drop()
 
 
-@pytest.mark.unit
-def test_database_connection(db):
-    assert db
+def test_create_valid_data(sut):
+    data_obj = {"firstName": "firstname", "lastName": "lastName", "email": "email@example.com"}
+    assert sut.create(data_obj)
 
+def test_create_invalid_data(sut):
+    data_obj = {"firstName": 1, "lastName": "lastName", "email": "email@example.com"}
+    assert sut.create(data_obj)
 
-@pytest.mark.unit
-def test_create_valid_data(dao):
-    '''Test creating a document with valid data'''
-    # Valid data for user collection
-    valid_data = {
-        "firstName": "testfirstname",
-        "lastName": "testlastname",
-        "email": "test.test@gmail.com"
-    }
-    # Using create method to insert given data
-    result = dao.create(valid_data)
-    assert result
+def test_create_missing_required_data(sut):
+    data_obj = {"firstName": "firstname", "email": "email@example.com"}
+    assert sut.create(data_obj)
 
-@pytest.mark.unit
-def test_create_invalid_data(dao):
-    dao.collection_name = "user"  # Set collection_name attribute
-    '''Test creating a document with invalid data'''
-    # Invalid data for user collection
-    invalid_data = {
-        "firstName": 1,
-        "lastName": "testlastname",
-        "email": "test.test@gmail.com"
-    }
-    result = dao.create(invalid_data)
-    
-    # Assert that the result is None when invalid data is provided
-    assert result
+def test_create_add_data(sut):
+    data_obj = {"firstName": "firstName", "lastName": "lastName", "email": "email@example.com", "added_value": "test"}
+    assert sut.create(data_obj)
 
-@pytest.mark.unit
-def test_create_add_to_valid_data(dao):
-    '''Test adding additional data to a valid document'''
-    # Valid data for user collection
-    valid_data = {
-        "firstName": "testfirstname",
-        "lastName": "testlastname",
-        "email": "test.test@gmail.com",
-        "testvalue": "testtesttest"
-    }
-    # Using create method to insert given data
-    result = dao.create(valid_data)
-    assert result  
+def test_create_empty_data(sut):
+    data_obj = {}
+    assert sut.create(data_obj)
 
-@pytest.mark.unit
-def test_create_missing_data(dao):
-    '''Test creating a document with a missing required properties'''
-    # Data with missing required properties for user collection
-    missing_data = {
-        "firstName": "testfirstname",
-        "lastName": "testlastname"
-    }
-    # Using create method to insert given data
-    result = dao.create(missing_data)
-    assert not result
-
-@pytest.mark.unit
-def test_create_unique_value(dao):
-    '''Test creating a document by inserting an un unique value'''
-    # Valid data for user collection
-    valid_data = {
-        "firstName": "testfirstname",
-        "lastName": "testlastname",
-        "email": "test.test@gmail.com"
-    }
-    # Using create method to insert given data
-    result = dao.create(valid_data)
-    assert result
-    result1 = dao.create(valid_data)
-    assert not result1
+def test_create_unique_data(sut):
+    data_obj_1 = { "firstName": "firstName", "lastName": "lastName", "email": "email@example.com"}
+    sut.create(data_obj_1)
+    data_obj_2 = { "firstName": "firstName", "lastName": "lastName", "email": "email@example.com"}
+    with pytest.raises(WriteError):
+        sut.create(data_obj_2)
